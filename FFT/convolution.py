8fb17179ba75b91eb2432642680d6213c4ab4ece
ahmedtawfiiiq <ahmedtawfiiiq24@gmail.com>
2023-11-02 09:28:53 +0200

    update poly convolution function

--- FFT/convolution.py
+++ FFT/convolution.py
@@ -1,17 +1,29 @@
-# a and b are of 3rd degree
-a_coef = [0, 0, 0, 6, 7, -10, 9][::-1]  # pad till size of c
-b_coef = [0, 0, 0, -2, 0, 4, -5][::-1]  # pad till size of c
-c_coef = []
+import numpy as np
 
-j = 0
-k = 0
-while j <= 6:  # c is of 6th degree
-    c = 0
-    while k <= j:
-        c += a_coef[k] * b_coef[j - k]
-        k += 1
-    c_coef.append(c)
-    j += 1
+
+def polynomial_convolution(a, b):
+    # perform polynomial multiplication using convolution
+    # a = (a0, a1, ..., an-1)
+    # b = (b0, b1, ..., bn-1)
+    na = len(a)
+    nb = len(b)
+    nc = na + nb - 1
+    # pad a and b with zeros to make them of same length of nc
+    a = np.pad(a, (0, nc - na))
+    b = np.pad(b, (0, nc - nb))
+    # perform convolution
+    j = 0
     k = 0
-c_coef = c_coef[::-1]
-print(c_coef)
+    c = np.zeros(nc)
+    while j < nc:
+        while k <= j:
+            c[j] += a[k] * b[j - k]
+            k += 1
+        j += 1
+        k = 0
+    return c
+
+
+a_coef = [9, -10, 7, 6]
+b_coef = [-5, 4, 0, -2]
+print(polynomial_convolution(a_coef, b_coef))
